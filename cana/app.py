126f7491c1faec11d25a9047ff43fe14a4e21641
Jonny Lamb <jonny@debian.org>
2012-09-26 13:57:20 +0200

    app: add an application ID and flags
    Signed-off-by: Jonny Lamb <jonny@debian.org>

--- cana/app.py
+++ cana/app.py
@@ -1,7 +1,8 @@
-from gi.repository import Gtk
+from gi.repository import Gtk, Gio
 
 class Application(Gtk.Application):
     def __init__(self):
-        Gtk.Application.__init__(self)
+        Gtk.Application.__init__(self, application_id='com.jonnylamb.Cana',
+                                 flags=Gio.ApplicationFlags.FLAGS_NONE)
 
         self.connect('activate', lambda a,b: None, None)
